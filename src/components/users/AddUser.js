26435957a89454abb05468bc98293e3c63355925
Jeff Ripke <jripke74@gmail.com>
2022-09-18 14:45:50 -0700

    add Helper/Wrapper.js

--- src/components/users/AddUser.js
+++ src/components/users/AddUser.js
@@ -3,6 +3,7 @@ import React, { useState, useRef } from 'react';
 import Card from '../UI/Card';
 import Button from '../UI/Button';
 import ErrorModal from '../UI/ErrorModal';
+import Wrapper from '../Helper/Wrapper';
 import classes from './AddUser.module.css';
 
 const AddUser = (props) => {
@@ -39,7 +40,7 @@ const AddUser = (props) => {
   };
 
   return (
-    <div>
+    <Wrapper>
       {error && (
         <ErrorModal
           title={error.title}
@@ -56,7 +57,7 @@ const AddUser = (props) => {
           <Button type="submit">Add User</Button>
         </form>
       </Card>
-    </div>
+    </Wrapper>
   );
 };
 
